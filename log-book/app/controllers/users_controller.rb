9477650ef0bef09abcb50941e76d9634a4af722e
github username <github email address>
2019-02-07 10:41:21 -0600

    writing the post to sign up and adding the failure view

--- log-book/app/controllers/users_controller.rb
+++ log-book/app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :signup
   end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
+  end
 end
