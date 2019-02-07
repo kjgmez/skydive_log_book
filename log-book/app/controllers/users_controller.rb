06f757fda81556d1e34bfc0199465676bd703027
github username <github email address>
2019-02-07 15:04:28 -0600

    sessions was enabled to use sessions[:user_id]

--- log-book/app/controllers/users_controller.rb
+++ log-book/app/controllers/users_controller.rb
@@ -5,18 +5,13 @@ class UsersController < ApplicationController
   end
 
   post '/user/signup' do
-    binding.pry
-    if params[:username] == "" || params[:password] == "" || User.find_by(username: params[:username]).name.empty? == false
-      redirect '/user/details'
+    #binding.pry
+    if params[:username] == "" || params[:password] == "" #|| User.find_by(username: params[:username]).name.empty? == false
+      redirect '/user/failure'
     else
       User.create(username: params[:username], password: params[:password])
       redirect '/user/login'
     end
-  end
-
-  get '/user/profile' do
-    @user = User.find(session[:user_id])
-    erb :'/user/profile'
   end
 
   get "/user/login" do
@@ -28,11 +23,17 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      #binding.pry
+      binding.pry
       redirect to "/user/profile"
     else
       redirect to "/user/failure"
     end
+  end
+
+  get '/user/profile' do
+    binding.pry
+    @user = User.find(session[:user_id])
+    erb :'/user/profile'
   end
 
   get "/user/failure" do
