db0930416d296c403ec8210445e809e4be11ff49
github username <github email address>
2019-02-07 09:54:08 -0600

    setting the views

--- log-book/app/controllers/application_controller.rb
+++ log-book/app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "l0gg_y0ur_jump"
+  set :views, Proc.new{File.join(root, "../views.")}
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
-helpers do
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def current_user
-    User.find(session[:user_id])
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 end
