d640f0581f0c41c416ba4c1605d7abfff57fcf03
github username <github email address>
2019-02-07 18:00:20 -0600

    raked migration to edit colum for jumps table

--- log-book/db/schema.rb
+++ log-book/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190207234714) do
+ActiveRecord::Schema.define(version: 20190207235156) do
 
   create_table "jumps", force: :cascade do |t|
     t.integer "jump_number"
     t.integer "altitude"
     t.string  "description"
-    t.integer "jumper_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
