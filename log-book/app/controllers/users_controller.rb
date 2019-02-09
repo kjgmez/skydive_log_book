6c319b4da322816f66219be06b77fbeaa2706612
github username <github email address>
2019-02-09 14:45:43 -0500

    adjusted wher helpers methods were lcated

--- log-book/app/controllers/users_controller.rb
+++ log-book/app/controllers/users_controller.rb
@@ -62,4 +62,14 @@ class UsersController < ApplicationController
     redirect "/welcome"
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
