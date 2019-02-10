d746babd8606677cdbb6b6633eff29aa10b06b13
github username <github email address>
2019-02-10 12:15:42 -0500

    added edit view and logic

--- log-book/app/controllers/jumps_controller.rb
+++ log-book/app/controllers/jumps_controller.rb
@@ -6,9 +6,22 @@ class JumpsController < ApplicationController
     erb :'/jump/new'
   end
 
-  get '/jump/:id' do
-    @jump = Jump.find(params[:id])
-   erb :show
+  post '/jump/index' do
+    @jump = Jump.create(params[:jump])
+    @jump.user_id = session[:user_id]
+    @jump.save
+    redirect to "/jump/#{@jump.id}"
   end
 
+  get '/jump/:id' do
+    binding.pry
+    @jump = Jump.find(params[:id])
+    erb :'/jump/show'
+  end
+
+  get '/jump/:id/edit' do
+    @jump = Jump.find(params[:id])
+    @user = User.find_by(session[:user_id])
+    erb :'/jump/edit'
+  end
 end
