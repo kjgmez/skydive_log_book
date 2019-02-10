343024542049c15562c337b4af2987b780c08560
github username <github email address>
2019-02-10 11:20:34 -0500

    jumper controller wiht new and find by id

--- log-book/app/controllers/jumps_controller.rb
+++ log-book/app/controllers/jumps_controller.rb
@@ -1,3 +1,13 @@
 class JumpsController < ApplicationController
 
+  get '/jump/new' do
+    @user = User.find_by(session[:user_id])
+    erb :'/jump/new'
+  end
+
+  get '/jump/:id' do
+    @jump = Jump.find(params[:id])
+   erb :show
+  end
+
 end
